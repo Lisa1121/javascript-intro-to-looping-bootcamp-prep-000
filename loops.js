a00b4b269f7feba7f7cbf7089508e29de84f8f43
Lisa Shaughnessy <lisavshaughnessy@gmail.com>
2018-03-21 03:57:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,8 @@
 function forLoop() {
   for (var i = 1; i<25; i++) {
     if (i===1) {
-      array.
+      console.log("I am 1 strange loop")
+      
     }
     
     console.log ("I am "+i+" strange loops.")
