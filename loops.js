623b5f486c8432113a44dc6b2a6b7f723e374e1f
Lisa Shaughnessy <lisavshaughnessy@gmail.com>
2018-03-21 03:58:14 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,9 +2,10 @@ function forLoop() {
   for (var i = 1; i<25; i++) {
     if (i===1) {
       console.log("I am 1 strange loop")
-      
-    }
+      }
+      else {
+      console.log ("I am "+i+" strange loops.")  
+      }
     
-    console.log ("I am "+i+" strange loops.")
   }
 }
